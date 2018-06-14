20b9b2b4905d5a50f598b5a8dc55563b923a52f2
Asen Lekov <asenlekoff@gmail.com>
2018-06-14 13:52:47 +0300

    Process using dekstop user agent by default

--- lib/html_analyzer.rb
+++ lib/html_analyzer.rb
@@ -15,11 +15,11 @@ module HtmlAnalyzer
   PHONE_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'.freeze
   DESKTOP_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'.freeze
 
-  def self.analyze(url, user_agent = PHONE_USER_AGENT)
+  def self.analyze(url, user_agent = DESKTOP_USER_AGENT)
     HtmlPage.process(url, user_agent)
   end
 
-  def self.modify(url, user_agent = PHONE_USER_AGENT)
+  def self.modify(url, user_agent = DESKTOP_USER_AGENT)
     HtmlPage.modify(url, user_agent)
   end
 
